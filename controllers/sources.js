a8a47956cd4c360d4bb2a094e5eff49e6221b0b0
Michael scharff <minka.scharff@gmail.com>
2023-09-05 15:03:29 +0300

    Fixing the function structure

--- controllers/sources.js
+++ controllers/sources.js
@@ -1,14 +1,13 @@
 const diskSpace = require('diskspace');
-const config = require('../config');
 const handleResponse = (res) => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`))
 
-module.exports.updateMemory = (whatDisk, ip) => {
+module.exports.updateMemory = (whatDisk, config) => {
     diskSpace.check(`${whatDisk}`, function (err, result) {
         if (!err) {
             if (result) {
                 let newData = { memoryLeft: result.free, totalMemory: result.total };
                 newData.lastChecked = new Date();
-                updateSource(ip, newData)
+                updateSource(config, newData)
                     .then((data) => {
                         if (data) {
                             console.log(data.message);
@@ -24,13 +23,13 @@ module.exports.updateMemory = (whatDisk, ip) => {
     });
 };
 
-module.exports.createServerEntry = (whatDisk, ip) => {
+module.exports.createServerEntry = (whatDisk, config) => {
     diskSpace.check(`${whatDisk}`, function (err, result) {
         if (!err) {
             if (result) {
-                let newData = { memoryLeft: result.free, totalMemory: result.total, collectionName: `${ip}` };
+                let newData = { memoryLeft: result.free, totalMemory: result.total, collectionName: `${config.address}` };
                 newData.checkedAt = new Date();
-                fetch(`http://${config.server}:${config.PORT}/add-entry`, {
+                fetch(`http://${config.server.hostname}:${config.server.PORT}/add-entry`, {
                     body: JSON.stringify(newData),
                     headers: {
                         'Content-Type': 'application/json',
@@ -53,20 +52,20 @@ module.exports.createServerEntry = (whatDisk, ip) => {
     });
 };
 
-module.exports.checkWebsite = (address, isEntry = false) => {
-    fetch(`http://${address}`)
+module.exports.checkWebsite = (config, isEntry = false) => {
+    fetch(`http://${config.address}`)
         .then((res) => {
             if (res.ok) {
                 const newData = { isActive: true, status: res.status };
                 newData.lastChecked = new Date();
                 newData.lastActive = new Date();
-                updateSource(address, newData)
+                updateSource(config, newData)
                     .then((data) => {
                         if (data.message === 'Successfully updated.') {
                             if (isEntry) {
-                                let dataObj = { status: 200, isActive: true, collectionName: `${address}` };
+                                let dataObj = { status: 200, isActive: true, collectionName: `${config.address}` };
                                 dataObj.checkedAt = new Date();
-                                createWebEntry(dataObj);
+                                createWebEntry(dataObj, config);
                             }
                         }
                     })
@@ -74,7 +73,7 @@ module.exports.checkWebsite = (address, isEntry = false) => {
                         if (err) {
                             const newData = { isActive: false, status: err.statusCode || 500 };
                             newData.lastChecked = new Date();
-                            updateSource(address, newData)
+                            updateSource(config, newData)
                                 .catch((err) => {
                                     if (err) {
                                         console.log(err);
@@ -83,9 +82,9 @@ module.exports.checkWebsite = (address, isEntry = false) => {
                         }
                     });
             } else {
-                const newData = { isActive: false, status: err.statusCode || 500 };
+                const newData = { isActive: false, status: res.statusCode || 500 };
                 newData.lastChecked = new Date();
-                updateSource(address, newData)
+                updateSource(config, newData)
                     .catch((err) => {
                         if (err) {
                             console.log(err);
@@ -97,7 +96,7 @@ module.exports.checkWebsite = (address, isEntry = false) => {
             if (err) {
                 const newData = { isActive: false, status: err.status || 500 };
                 newData.lastChecked = new Date();
-                updateSource(address, newData)
+                updateSource(config, newData)
                     .catch((err) => {
                         if (err) {
                             console.log(err);
@@ -107,8 +106,19 @@ module.exports.checkWebsite = (address, isEntry = false) => {
         });
 };
 
-const updateSource = (address, data) => {
-    return fetch(`http://${config.server}:${config.PORT}/update/${address}`, {
+module.exports.createResource = (config) => {
+    fetch(`http://${config.server.hostname}:${config.server.PORT}/add-source`, {
+        body: JSON.stringify({ name: config.name, url: config.address, lastActive: new Date('1-1-1970'), isActive: false, status: 404, lastChecked: new Date("1-1-1970") }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        method: 'POST',
+    }).then(handleResponse)
+};
+
+const updateSource = (config, data) => {
+    return fetch(`http://${config.server.hostname}:${config.server.PORT}/update/${config.name}`, {
         body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json',
@@ -118,8 +128,8 @@ const updateSource = (address, data) => {
     }).then(handleResponse)
 };
 
-const createWebEntry = (body) => {
-    fetch(`http://${config.server}:${config.PORT}/add-entry`, {
+const createWebEntry = (body, config) => {
+    fetch(`http://${config.server.hostname}:${config.server.PORT}/add-entry`, {
         body: JSON.stringify(body),
         headers: {
             'Content-Type': 'application/json',
