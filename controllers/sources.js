65fe967dfa041d33cc3879c6ba110a95715bc379
Michael scharff <minka.scharff@gmail.com>
2023-09-20 14:06:56 +0300

    Refactoring the sources.js functions and implementing the shouldUpdate property

--- controllers/sources.js
+++ controllers/sources.js
@@ -2,57 +2,21 @@ const diskSpace = require('diskspace');
 const os = require('os');
 const handleResponse = (res) => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`))
 
-module.exports.updateMemory = (whatDisk, config) => {
-    diskSpace.check(`${whatDisk}`, function (err, result) {
-        if (!err) {
-            if (result) {
-                const freeMem = os.freemem();
-                const totalMem = os.totalmem();
-                let newData = { capacityLeft: result.free, totalCapacity: result.total, totalMemory: totalMem, freeMemory: freeMem };
-                newData.lastChecked = new Date();
-                updateSource(config, newData)
-                    .then((data) => {
-                        if (data) {
-                            console.log(data.message);
-                        }
-                    })
-                    .catch((err) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
+module.exports.checkMemory = (whatDisk, config, isEntry = false) => {
+    diskSpace.check(`${whatDisk}`, async (err, result) => {
+        if (!err && result) {
+            const freeMem = os.freemem();
+            const totalMem = os.totalmem();
+            let newData = { capacityLeft: result.free, totalCapacity: result.total, totalMemory: totalMem, freeMemory: freeMem };
+            newData.checkedAt = new Date();
+            if (config.shouldUpdate) {
+                updateSource(config, newData);
             }
-        }
-    });
-};
+            if (isEntry) {
+                newData.collectionName = `${config.address}`;
+                createEntry(newData, config);
+            }
 
-module.exports.createServerEntry = (whatDisk, config) => {
-    diskSpace.check(`${whatDisk}`, function (err, result) {
-        if (!err) {
-            if (result) {
-                const freeMem = os.freemem();
-                const totalMem = os.totalmem();
-                let newData = { capacityLeft: result.free, totalCapacity: result.total, collectionName: `${config.address}`, totalMemory: totalMem, freeMemory: freeMem };
-                newData.checkedAt = new Date();
-                fetch(`http://${config.server.hostname}:${config.server.PORT}/add-entry`, {
-                    body: JSON.stringify(newData),
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',
-                    },
-                    method: 'POST',
-                }).then(handleResponse)
-                    .then((data) => {
-                        if (data) {
-                            console.log(data.message);
-                        }
-                    })
-                    .catch((err) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
-            }
         }
     });
 };
@@ -64,13 +28,13 @@ module.exports.checkWebsite = (config, isEntry = false) => {
                 const newData = { isActive: true, status: res.status };
                 newData.lastChecked = new Date();
                 newData.lastActive = new Date();
-                updateSource(config, newData)
+                updateSource(config, newData, true)
                     .then((data) => {
                         if (data.message === 'Successfully updated.') {
                             if (isEntry) {
                                 let dataObj = { status: 200, isActive: true, collectionName: `${config.address}` };
                                 dataObj.checkedAt = new Date();
-                                createWebEntry(dataObj, config);
+                                createEntry(dataObj, config);
                             }
                         }
                     })
@@ -79,32 +43,22 @@ module.exports.checkWebsite = (config, isEntry = false) => {
                             if (isEntry) {
                                 let dataObj = { status: 400, isActive: true, collectionName: `${config.address}` };
                                 dataObj.checkedAt = new Date();
-                                createWebEntry(dataObj, config);
+                                createEntry(dataObj, config);
                             }
                             const newData = { isActive: true, status: 400 };
                             newData.lastChecked = new Date();
-                            updateSource(config, newData)
-                                .catch((err) => {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-                                });
+                            updateSource(config, newData);
                         }
                     });
             } else {
                 if (isEntry) {
                     let dataObj = { status: 500, isActive: true, collectionName: `${config.address}` };
                     dataObj.checkedAt = new Date();
-                    createWebEntry(dataObj, config);
+                    createEntry(dataObj, config);
                 }
                 const newData = { isActive: true, status: 500 };
                 newData.lastChecked = new Date();
-                updateSource(config, newData)
-                    .catch((err) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
+                updateSource(config, newData);
             }
         })
         .catch((err) => {
@@ -112,28 +66,18 @@ module.exports.checkWebsite = (config, isEntry = false) => {
                 if (isEntry) {
                     let dataObj = { status: 404, isActive: false, collectionName: `${config.address}` };
                     dataObj.checkedAt = new Date();
-                    createWebEntry(dataObj, config);
+                    createEntry(dataObj, config);
                 }
                 const newData = { isActive: false, status: 404 };
                 newData.lastChecked = new Date();
-                updateSource(config, newData)
-                    .then((data) => {
-                        if (data) {
-
-                        }
-                    })
-                    .catch((err) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
+                updateSource(config, newData);
             }
         });
 };
 
 module.exports.createResource = (config) => {
     fetch(`http://${config.server.hostname}:${config.server.PORT}/add-source`, {
-        body: JSON.stringify({ name: config.name, url: config.address, lastActive: new Date('1-1-1970'), isActive: false, status: 404, lastChecked: new Date("1-1-1970") }),
+        body: JSON.stringify({ name: config.name, url: config.address, lastActive: new Date('1-1-1970'), isActive: false, status: 404, lastChecked: new Date("1-1-1970"), isMachine: !config.shouldUpdate }),
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
@@ -147,18 +91,39 @@ module.exports.createResource = (config) => {
         });
 };
 
-const updateSource = (config, data) => {
-    return fetch(`http://${config.server.hostname}:${config.server.PORT}/update/${config.name}`, {
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        },
-        method: 'PUT',
-    }).then(handleResponse)
+const updateSource = (config, data, isThen = false) => {
+    if (!isThen) {
+        fetch(`http://${config.server.hostname}:${config.server.PORT}/update/${config.name}`, {
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            method: 'PUT',
+        }).then(handleResponse)
+            .then((data) => {
+                if (data) {
+                    console.log(data);
+                }
+            })
+            .catch((err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+    } else {
+        return fetch(`http://${config.server.hostname}:${config.server.PORT}/update/${config.name}`, {
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            method: 'PUT',
+        }).then(handleResponse)
+    }
 };
 
-const createWebEntry = (body, config) => {
+const createEntry = (body, config) => {
     fetch(`http://${config.server.hostname}:${config.server.PORT}/add-entry`, {
         body: JSON.stringify(body),
         headers: {
